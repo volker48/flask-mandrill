61ea65b5e7d0d418f2a54459e9450b1b931ac7a7
Marcus McCurdy <mmccurdy@50onred.com>
2013-10-25 13:27:54 -0400

    Updates setup.py adds history

---
+++ HISTORY.rst
@@ -0,0 +1,14 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.1.1 (2013-10-25)
+++++++++++++++++++
+
+- Added config option MANDRILL_DEFAULT_FROM.
+
+0.1 (2012-10-25)
+++++++++++++++++
+
+- First release.
--- setup.py
+++ setup.py
@@ -1,15 +1,48 @@
+"""
+Flask-Mandrill
+--------------
+
+A Flask Extension to remove some of the boiler plate encountered when
+sending emails with `Mandrill <http://www.mandrill.com/>`_
+
+Installation
+````````````
+
+.. code:: bash
+    $ pip install flask-mandrill
+
+
+Usage
+`````
+
+.. code:: python
+
+        from flask import Flask
+        from from flask.ext.mandrill import Mandrill
+
+        app = Flask(__name__)
+        app.config['MANDRILL_API_KEY'] = 'your api key'
+        mandrill = Mandrill(app)
+        mandrill.send_email(
+            from_email='someone@yourdomain.com',
+            to=[{'email': 'someoneelse@someotherdomain.com'}],
+            text='Hello World'
+        )
+"""
+
 from setuptools import setup
 
 
 setup(
     name='Flask-Mandrill',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/volker48/flask-mandrill',
     license='MIT',
     author='Marcus McCurdy',
     author_email='marcus.mccurdy@gmail.com',
     description='Adds Mandrill support to Flask applications',
-    long_description=__doc__,
+    long_description=__doc__ + '\n\n' +
+                     open('HISTORY.rst').read(),
     py_modules=['flask_mandrill'],
     zip_safe=False,
     platforms='any',
